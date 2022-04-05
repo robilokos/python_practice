8a8411ab9ec59d62a8511eed04108ca28ced87bc
Lőkös Róbert <robilokos93@gmail.com>
2022-04-05 17:22:23 +0200

    RL create first practice file

---
+++ Variables and data types/practice1.py
@@ -0,0 +1,33 @@
+print("Változók létrehozása és adat típusok gyakorlása")
+
+# Szöveg típus: str
+# Szám típusok: int, float, complex
+# Szekvencia típusok: list, tuple, range
+# Mapping típus: dict
+# Halmaz típusok: set, frozenset
+# Bool típus: bool
+# Bináris típusok: bytes, bytearray, memoryview
+# Típus lekérdezése: type(var)
+
+print("Név változó létrehozása.\n")
+
+
+
+print("Életkor változó létrehozása.\n")
+
+
+
+print(f"Név: , Életkor: \n")
+
+
+
+print("Változók típusának lekérdezése.\n")
+
+
+
+print("Több változó létrehozása egyszerre.\n")
+
+
+
+print("Több egyforma értékű változó létrehozása egyszerre.\n")
+
